192ebb887caac2ce23ac3da7bbf7c5f17551be5b
Yeongwoo Kim <yw1221@g.skku.edu>
2023-12-31 08:59:15 +0900

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by YeongWooKim on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
